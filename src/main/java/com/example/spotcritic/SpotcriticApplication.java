b51f7784b6f78c18fce9b2725d071f3eb2fcd9bc
andremeneses0103 <andremeneses0103@gmail.com>
2024-09-07 22:21:21 -0300

    First Commit

---
+++ src/main/java/com/example/spotcritic/SpotcriticApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotcritic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotcriticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotcriticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spotcritic/SpotcriticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotcritic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotcriticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
